9deb1ddb4c59e2ea4329b40345c163f906d9cc13
i.manikhin <i.manikhin@vk.team>
2022-07-15 01:17:51 +0300

    Updated main urls file in project

--- articles_editor/urls.py
+++ articles_editor/urls.py
@@ -4,9 +4,9 @@ from django.urls import include, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    re_path(r'admin/', admin.site.urls),
-    re_path(r'token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    re_path(r'token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     re_path(r'^api/v1/', include('users.urls')),
     re_path(r'^api/v1/', include('articles.urls')),
     re_path(r'^api/v1/', include('tags.urls'))
